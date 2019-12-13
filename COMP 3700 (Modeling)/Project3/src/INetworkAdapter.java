d007da57aad23e2ad6db990b6d2f01cf14e9fa8e
Alex Lewin <asl0028@auburn.edu>
2019-12-12 21:46:32 -0600

    updates

---
+++ COMP 3700 (Modeling)/Project3/src/INetworkAdapter.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+
+public interface INetworkAdapter {
+    public String exchange(String msg, String host, int port) throws Exception;
+
+    public MessageModel exchange(MessageModel msg, String host, int port) throws Exception;
+}
---
+++ COMP 3700 (Modeling)/Project3/src/ManageCustomerUI.java
@@ -0,0 +1,171 @@
+import com.google.gson.Gson;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ManageCustomerUI {
+
+    public JFrame view;
+
+    public JButton btnLoad = new JButton("Load Customer");
+    public JButton btnSave = new JButton("Save Customer");
+
+    public JTextField txtCustomerID = new JTextField(20);
+    public JTextField txtName = new JTextField(20);
+    public JTextField txtPhone = new JTextField(20);
+    public JTextField txtAddress = new JTextField(20);
+
+
+    public ManageCustomerUI() {
+        this.view = new JFrame();
+
+        view.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        view.setTitle("Manage Customer Information");
+        view.setSize(600, 400);
+        view.getContentPane().setLayout(new BoxLayout(view.getContentPane(), BoxLayout.PAGE_AXIS));
+
+        JPanel panelButtons = new JPanel(new FlowLayout());
+        panelButtons.add(btnLoad);
+        panelButtons.add(btnSave);
+        view.getContentPane().add(panelButtons);
+
+        JPanel line1 = new JPanel(new FlowLayout());
+        line1.add(new JLabel("CustomerID "));
+        line1.add(txtCustomerID);
+        view.getContentPane().add(line1);
+
+        JPanel line2 = new JPanel(new FlowLayout());
+        line2.add(new JLabel("Name "));
+        line2.add(txtName);
+        view.getContentPane().add(line2);
+
+        JPanel line3 = new JPanel(new FlowLayout());
+        line3.add(new JLabel("Phone "));
+        line3.add(txtPhone);
+        view.getContentPane().add(line3);
+
+        JPanel line4 = new JPanel(new FlowLayout());
+        line4.add(new JLabel("Address "));
+        line4.add(txtAddress);
+        view.getContentPane().add(line4);
+
+
+        btnLoad.addActionListener(new LoadButtonListerner());
+
+        btnSave.addActionListener(new SaveButtonListener());
+
+    }
+
+    public void run() {
+        view.setVisible(true);
+    }
+
+    class LoadButtonListerner implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            Gson gson = new Gson();
+            String id = txtCustomerID.getText();
+
+            if (id.length() == 0) {
+                JOptionPane.showMessageDialog(null, "CustomerID cannot be null!");
+                return;
+            }
+
+            try {
+                int i = Integer.parseInt(id);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "CustomerID is invalid!");
+                return;
+            }
+
+
+            try {
+
+                MessageModel msg = new MessageModel();
+                msg.code = MessageModel.GET_CUSTOMER;
+                msg.data = id;
+
+//                msg = StoreManager.getInstance().getNetworkAdapter().send(msg, "localhost", 1000);
+
+                if (msg.code == MessageModel.OPERATION_FAILED) {
+                    JOptionPane.showMessageDialog(null, "Customer NOT exists!");
+                }
+                else {
+                    CustomerModel customer = gson.fromJson(msg.data, CustomerModel.class);
+                    txtName.setText(customer.mName);
+                    txtPhone.setText(customer.mPhone);
+                    txtAddress.setText(customer.mAddress);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    class SaveButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            ProductModel product = new ProductModel();
+            Gson gson = new Gson();
+            String id = txtCustomerID.getText();
+
+            if (id.length() == 0) {
+                JOptionPane.showMessageDialog(null, "ProductID cannot be null!");
+                return;
+            }
+
+            try {
+                product.mProductID = Integer.parseInt(id);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "ProductID is invalid!");
+                return;
+            }
+
+            String name = txtName.getText();
+            if (name.length() == 0) {
+                JOptionPane.showMessageDialog(null, "Product name cannot be empty!");
+                return;
+            }
+
+            product.mName = name;
+
+            String price = txtPhone.getText();
+            try {
+                product.mPrice = Double.parseDouble(price);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Price is invalid!");
+                return;
+            }
+
+            String quant = txtAddress.getText();
+            try {
+                product.mQuantity = Double.parseDouble(quant);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Quantity is invalid!");
+                return;
+            }
+
+            try {
+                MessageModel msg = new MessageModel();
+                msg.code = MessageModel.PUT_PRODUCT;
+                msg.data = gson.toJson(product);
+
+//                msg = StoreManager.getInstance().getNetworkAdapter().send(msg, "localhost", 1000);
+
+                if (msg.code == MessageModel.OPERATION_FAILED)
+                    JOptionPane.showMessageDialog(null, "Product is NOT saved successfully!");
+                else
+                    JOptionPane.showMessageDialog(null, "Product is SAVED successfully!");
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
